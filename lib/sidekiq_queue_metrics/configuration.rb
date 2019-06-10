a8cf7c015430e0340e3a670c6bc424b3b51bf1b1
Ivan Etchart <ivan.etchart@me.com>
2019-06-10 16:38:29 -0300

    Support older sidekiq versions
    Currently, using death_handlers it was implicitly forcing to use sidekiq version >= 5.1.

--- lib/sidekiq_queue_metrics/configuration.rb
+++ lib/sidekiq_queue_metrics/configuration.rb
@@ -1,10 +1,24 @@
 module Sidekiq::QueueMetrics
+  def self.support_death_handlers?
+    Sidekiq::VERSION >= '5.1'
+  end
+
   def self.init(config)
     config.server_middleware do |chain|
       chain.add Sidekiq::QueueMetrics::JobSuccessMonitor
     end
 
-    config.death_handlers << Sidekiq::QueueMetrics::JobDeathMonitor.proc
+    if support_death_handlers?
+      config.death_handlers << Sidekiq::QueueMetrics::JobDeathMonitor.proc
+    else
+      workers = ObjectSpace
+        .each_object(Class)
+        .select { |c| c.included_modules.include?(Sidekiq::Worker) && !c.to_s.start_with?('Sidekiq::') }
+
+      workers.each do |worker|
+        worker.instance_eval { sidekiq_retries_exhausted(&Sidekiq::QueueMetrics::JobDeathMonitor.proc) }
+      end
+    end
   end
 
   def self.storage_location=(key)
