bef0494544c2abb6ac6164bb6dcc572667aede12
Ivan Etchart <ivan.etchart@me.com>
2019-07-19 15:51:22 -0300

    Improve metrics processing
    * Use redis hash data type instead of json payloads
    * Fix issue at the web extension when a new queue is added
    * Improve tests to avoid using expectations when possible

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :rubygems
 
 sidekiq_version = ENV.fetch('SIDEKIQ_VERSION', '> 0')
 
+gemspec
+
 gem 'sidekiq', sidekiq_version
 gem 'eldritch'
-gem 'rspec'
--- lib/sidekiq_queue_metrics/configuration.rb
+++ lib/sidekiq_queue_metrics/configuration.rb
@@ -8,6 +8,10 @@ module Sidekiq::QueueMetrics
       chain.add Sidekiq::QueueMetrics::JobSuccessMonitor
     end
 
+    config.on(:startup) do
+      upgrade_to_v3 if upgrade_needed?
+    end
+
     if support_death_handlers?
       config.death_handlers << Sidekiq::QueueMetrics::JobDeathMonitor.proc
     else
@@ -19,6 +23,45 @@ module Sidekiq::QueueMetrics
         worker.instance_eval { sidekiq_retries_exhausted(&Sidekiq::QueueMetrics::JobDeathMonitor.proc) }
       end
     end
+  end
+
+  def self.upgrade_needed?
+    Sidekiq.redis_pool.with { |conn| conn.exists(Helpers.stats_key) }
+  end
+
+  def self.logger
+    @@logger ||= Logger.new(STDOUT)
+  end
+
+  def self.upgrade_to_v3
+    logger.info('Starting sidekiq_queue_metrics v3 upgrade')
+
+    Sidekiq.redis_pool.with do |conn|
+      old_collected_metrics = JSON.load(conn.get(Helpers.stats_key))
+      old_collected_metrics.each do |(queue, stats)|
+        logger.info("Upgrading #{queue} statistics")
+
+        stats.each { |(stat, value)| Sidekiq::QueueMetrics::Storage.increment_stat(queue, stat, value) }
+
+        failed_jobs_key = Helpers.build_failed_jobs_key(queue)
+
+        if conn.exists(failed_jobs_key) && conn.type(failed_jobs_key) == 'string'
+          temporal_failed_key = "_#{failed_jobs_key}"
+
+          failed_jobs = JSON.parse(conn.get(Helpers.build_failed_jobs_key(queue)) || '[]')
+
+          conn.rename(failed_jobs_key, temporal_failed_key)
+
+          failed_jobs.each { |job| Sidekiq::QueueMetrics::Storage::add_failed_job(job) }
+
+          conn.del(temporal_failed_key)
+        end
+      end
+
+      conn.del(Helpers.stats_key)
+    end
+
+    logger.info("Sucessfully upgraded")
   end
 
   def self.storage_location=(key)
